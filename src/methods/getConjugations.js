0c554fcb38db4adaa9387b66eb4767546de2fe0d
Bluskyo <blusky445@gmail.com>
2024-10-11 16:40:20 +0200

    using context

---
+++ src/methods/getConjugations.js
@@ -0,0 +1,41 @@
+// should be same as Settings's checkbox default.
+
+var defaultSettings = {        
+    affirmative : true,
+    negative: false,
+    formal: true,
+    informal: false, 
+    present: true,
+    past: false
+}
+
+export function getConjugation(data, settings = defaultSettings) {
+    console.log(settings);
+    const encodedKanji = encodeURIComponent(data.kanji);
+    const encodedTag = encodeURIComponent(data.tag);
+
+    return fetch('http://localhost:8080/settings', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+            'tag': encodedTag,
+            'kanji': encodedKanji
+        },
+        body: JSON.stringify(settings),
+    })
+    .then(response => {
+        if (!response.ok) {
+            throw new Error(`HTTP error! status: ${response.status}`);
+        }
+        return response.json();
+    })
+    .then(data => {
+        console.log('Success:', data);
+        return data;
+    })
+    .catch((error) => {
+        console.error('Error:', error);
+    });
+}
+
+export default getConjugation;
